c52644bd009f253419b6fc39d9a388be38e45f48
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 04:24:53 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,7 +46,6 @@ def movies_with_director_key(name, movies_collection)
 
   result
 end
- end
  
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
