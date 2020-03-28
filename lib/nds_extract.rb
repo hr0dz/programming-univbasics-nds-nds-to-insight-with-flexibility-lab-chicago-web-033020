af700e6e9eeb8cebc0c214f7a519113b1b04cf4b
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 04:01:32 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -42,6 +42,7 @@ def movies_with_director_key(name, movies_collection)
    new_array << movie_with_director_name(name, movie_data)
    collect_index += 1
    end
+   binding.pry
   new_array
  end
  
