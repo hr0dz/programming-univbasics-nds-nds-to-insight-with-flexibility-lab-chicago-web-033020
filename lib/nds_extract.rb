fbefa032b1e87b9db116344d348cc4d1e28a337d
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 04:31:00 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -104,7 +104,7 @@ def movies_with_directors_set(source)
     dir_info_hash = source[i]
     director_name = dir_info_hash[:name]
     directors_movies = dir_info_hash[:movies]
-    a_o_a_movies_by_dir << movies_with_director_key(director, source)
+    a_o_a_movies_by_dir << movies_with_director_key(di)
     i += 1
   end
 
