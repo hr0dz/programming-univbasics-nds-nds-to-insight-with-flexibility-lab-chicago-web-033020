87ae43108ccc6a282ac4cb90b32d0b8e5440440a
Haley Rodzos <haley.rodzos@gmail.com>
2020-03-28 03:51:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,6 @@
 # Provided, don't edit
 require 'directors_database'
+require 'pry'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
 # [3,4,5], [6]] => [1,2,3,4,5,6].
@@ -34,6 +35,19 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  dir_array = []
+  collect_index = 0 
+  while collect_index < movies_collection.length do 
+    dir_hash = {}
+    dir_hash[:director_name] = name
+    dir_hash[:title] = movies_collection[collect_index][:title]
+    dir_array.push(dir_hash)
+    collect_index += 1 
+   end
+   binding.pry
+   dir_array
+ end
+ 
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -48,10 +62,27 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-end
 
 
 def gross_per_studio(collection)
+collect_index = 0 
+  studio_hash = {}
+  idfk = ""
+ while collect_index < collection.length do 
+   studio = collection[collect_index][:studio]
+    gross = collection[collect_index][:worldwide_gross]
+    #studio_hash << movie_with_director_name
+    if !studio_hash[studio]
+       studio_hash[studio] = gross
+    else 
+       studio_hash[studio] += gross
+   end
+     collect_index += 1
+   end
+   studio_hash
+end
+
+
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
   # each studio.
@@ -63,9 +94,31 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_gross numbers for every movie in the input Hash
-end
 
 def movies_with_directors_set(source)
+ # Array of Arrays containing all of a director's movies. Each movie will need
+  # to have a :director_name key added to it.
+ 
+ index = 0
+  movies_for_array = Array.new
+  
+  while index < source.length do
+    director = source[index][:name]
+    movie_titles = source[index][:movies]
+    # movies_temp = movie_with_director_name(director, source[index][:movies])
+    movies_for_array.push(movies_with_director_key(director, source))
+    index += 1
+  end
+  binding.pry
+  movies_for_array
+end
+    
+#def directors_database()
+  #allstudios = studios_totals(directors_database)
+  #allstudios
+#end
+
+    
   # GOAL: For each director, find their :movies Array and stick it in a new Array
   #
   # INPUT:
@@ -76,7 +129,6 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.
-end
 
 # ----------------    End of Your Code Region --------------------
 # Don't edit the following code! Make the methods above work with this method
